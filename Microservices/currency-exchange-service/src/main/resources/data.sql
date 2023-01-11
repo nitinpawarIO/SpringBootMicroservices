bcda0b9d6d36a8968afc35f6e65ab18ad44a5ae9
nitinpawarIO <61464058+nitinpawarIO@users.noreply.github.com>
2023-01-11 18:44:35 +0530

    created new services and naming server

---
+++ Microservices/currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(1000,'USD','INR',81,'');
+
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(1001,'EUR','INR',75,'');
+
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(1002,'AUD','INR',50,'');
